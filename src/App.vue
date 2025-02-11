38c33ddf0a8e656118d9b2b83911f890e2c675b7
Dokong2 <jiwan6942@gmail.com>
2025-02-11 15:57:18 +0900

    week 1 + homework

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
+
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p>{{ prices[0] }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ prices[1]  }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ prices[2]  }} 만원</p>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      prices : ["70", "110", "30"],
+      products : ["역삼동원룸", "천호동원룸", "마포구원룸룸"]
+    }
   }
 }
 </script>
