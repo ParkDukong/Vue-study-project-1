102856d59df34106008386b92e3697c9f4d968ec
Dokong2 <jiwan6942@gmail.com>
2025-02-12 22:46:35 +0900

    final vue comit!

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,73 @@
+import get from "./assets/post.js";
+
 <template>
+
+  <div class="black-bg" v-if="modal_open == true">
+    <div class="white-bg">
+      <h4>페이지임</h4> 
+      <p>내용임</p>
+      <button @click="modal_open = false">닫기</button>
+    </div>
+  </div>
 
   <div class="menu">
     <a v-for="text in menus" :key="text">{{text}}</a>
     <a>Products</a>
     <a>About</a>
   </div>
-  <div v-for="a, i in products" :key="i">
-    <h4>{{ a }}</h4>
-    <p>{{ prices[i] }} 만원</p>
+  <div v-for="a, i in onerooms" :key="i">
+    <img class="image" src=a.image>
+    <h4 @click="modal_open = true">{{ a.title }}</h4>
+    <p>{{ a.price }} 만원</p>
+    <p>{{ a.content }}</p>
     <button @click="reports[i]++">허위매물신고</button> <span>신고수 : {{ reports[i] }}</span>
   </div>
 
 </template>
 
 <script>
+import data from "./assets/post.js";
+
 
 export default {
   name: 'App',
   data() {
     return {
+      onerooms : data,
       reports : [0, 0, 0],
       menus : ["Home", "Shop", "About"],
       prices : ["70", "110", "30"],
-      products : ["역삼동원룸", "천호동원룸", "마포구원룸룸"]
+      products : ["역삼동원룸", "천호동원룸", "마포구원룸룸"],
+      modal_open : false
+    }
+  },
+  methods : {
+    function_name() {
+
     }
   }
 }
+
 </script>
 
 <style>
+body {
+  margin: 0
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
